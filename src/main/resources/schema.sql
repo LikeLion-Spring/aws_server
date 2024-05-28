1c9693d6f2aa1f8dd816c7cff5f4f25d227065e3
sam971114 <simonana@naver.com>
2024-05-28 16:35:49 +0900

    feat: table명 재수정

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS users (
+CREATE TABLE IF NOT EXISTS USERS (
                                         id INT AUTO_INCREMENT PRIMARY KEY,
                                         name VARCHAR(255) NOT NULL,
                                         email VARCHAR(255) NOT NULL
