43ca20f3d7afb52099d4dc5ea3f708b343856efd
sam971114 <simonana@naver.com>
2024-05-28 16:40:56 +0900

    feat: sql문 수정

--- src/main/java/com/likelion/testserver/entity/User.java
+++ src/main/java/com/likelion/testserver/entity/User.java
@@ -11,7 +11,7 @@ import lombok.Setter;
 @Entity
 @Getter
 @Setter
-@Table(name = "users")
+@Table(name = "USERS")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -2,4 +2,5 @@ CREATE TABLE IF NOT EXISTS USERS (
                                         id INT AUTO_INCREMENT PRIMARY KEY,
                                         name VARCHAR(255) NOT NULL,
                                         email VARCHAR(255) NOT NULL
-    );
+);
+INSERT INTO USERS (id, name, email) VALUES (1, 'test','Example');
